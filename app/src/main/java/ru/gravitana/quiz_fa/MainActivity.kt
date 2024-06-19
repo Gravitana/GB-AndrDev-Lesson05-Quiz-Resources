5b5855fbd441652c20a037f93becf7015a39e6ca
Gravitana <v.a.grahov@yandex.ru>
2024-06-19 23:21:09 +0500

    viewBinding

--- app/src/main/java/ru/gravitana/quiz_fa/MainActivity.kt
+++ app/src/main/java/ru/gravitana/quiz_fa/MainActivity.kt
@@ -1,20 +1,19 @@
 package ru.gravitana.quiz_fa
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import ru.gravitana.quiz_fa.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.welcome.text = buildString {
+            append(getString(R.string.app_name))
+            append(" ")
+            append(getString(R.string.app_version))
         }
     }
 }
